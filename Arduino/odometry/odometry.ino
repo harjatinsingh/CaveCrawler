554795c1f2fe81598b42f08794c5895d9eb0f705
David Robinson <dvsrobinson@gmail.com>
2018-03-15 11:40:06 -0400

    added odom publisher

--- Arduino/odometry/odometry.ino
+++ Arduino/odometry/odometry.ino
@@ -1,125 +1,104 @@
-/*
- * rosserial Publisher Example
- * Prints "hello world!"
- */
+/* 
+* Arduino ROS Node for Winebot 
+* Author: David Robinson
+*/
 
 #include <ros.h>
-#include <std_msgs/String.h>
+#include <ros/time.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
 
 ros::NodeHandle nh;
 
-std_msgs::String str_msg;
-ros::Publisher chatter("chatter", &str_msg);
+geometry_msgs::TransformStamped t;
+tf::TransformBroadcaster broadcaster;
 
-char hello[13] = "hello world!";
+double x = 1.0;
+double y = 0.0;
+double theta = 1.57;
 
-void setup()
-{
-  Serial.begin(115200);
-  nh.initNode();
-  nh.advertise(chatter);
+double left_odom = 0;
+double left_turn = 0;
+double right_odom = 0;
+double right_turn = 0; 
+
+char base_link[] = "/base_link";
+char odom[] = "/odom";
+
+long int lastPubTime = 0;
+int dataRate  = 100; // ms or 10Hz
+
+#include <Encoder.h>
+Encoder myEnc(9, 10);
+
+bool debug = false; // Switch Serial Print / ROS Publisher
+
+void setup() {
+  if (debug) {
+    Serial.begin(9600);
+  }
+  else {
+    nh.initNode();
+    broadcaster.init(nh);
+  }
 }
 
-void loop()
-{
-  str_msg.data = hello;
-  chatter.publish( &str_msg );
-  nh.spinOnce();
-  delay(1000);
+void loop() {
+
+  read_sensors();
+  
+  if (debug) { // Serial Print
+      Serial.print("\t| LEFT | Odom: ");
+      Serial.print(left_odom);
+      Serial.print("\tTurn: ");
+      Serial.print(left_turn);
+      Serial.print("\t| RIGHT | Odom: ");
+      Serial.print(right_odom);
+      Serial.print("\tTurn: ");
+      Serial.print(right_turn);
+      Serial.println("");
+  }
+  else { // ROS Publisher
+
+    long int time = millis();
+    double dt = (time - lastPubTime);
+    if (dt > dataRate) { // attenuate rate
+  
+      // drive in a circle
+      double dx = 0.2;
+      double dtheta = 0.18;
+      x += cos(theta)*dx*0.1;
+      y += sin(theta)*dx*0.1;
+      theta += dtheta*0.1;
+      if(theta > 3.14)
+        theta=-3.14;
+        
+      // tf odom->base_link
+      t.header.frame_id = odom;
+      t.child_frame_id = base_link;
+      
+      t.transform.translation.x = x;
+      t.transform.translation.y = y;
+      
+      t.transform.rotation = tf::createQuaternionFromYaw(theta);
+      t.header.stamp = nh.now();
+      
+      broadcaster.sendTransform(t);
+      nh.spinOnce();
+  
+    }
+  }
 }
 
-///* 
-//* Arduino ROS Node for Winebot 
-//* Author: David Robinson
-//*/
-//
-//#include <ros.h>
-//#include <std_msgs/Float32MultiArray.h>
-//
-//#include <Encoder.h>
-//Encoder myEnc(9, 10);
-//
-//bool debug = false; // Change this to begin ROS coms
-//
-//void setup() {
-//  if (debug) {
-//    Serial.begin(115200);
-//  }
-//  else setupROS();
-//}
-//
-//float left_odom = 0;
-//float left_turn = 0;
-//float right_odom = 0;
-//float right_turn = 0; 
-//
-//void loop() {
-// 
-////  long newPosition = myEnc.read();
-//  
-//  left_odom = left_odom + 1;
-//  left_turn = left_turn + 1;
-//  right_odom = right_odom + 1;
-//  right_turn = right_turn + 1;
-//  
-//  if (debug) {
-//      Serial.print("\t| LEFT | Odom: ");
-//      Serial.print(left_odom);
-//      Serial.print("\tTurn: ");
-//      Serial.print(left_turn);
-//      Serial.print("\t| RIGHT | Odom: ");
-//      Serial.print(right_odom);
-//      Serial.print("\tTurn: ");
-//      Serial.print(right_turn);
-//      Serial.println("");
-//  }
-//  else runROS();
-//}
-//
-//// ROS Functions
-//
-//ros::NodeHandle nh;
-//static int messageLength = 4;
-//std_msgs::Float32MultiArray odometry;
-//ros::Publisher odometry_pub("odometry", &odometry);
-//
-//void setupROS() {
-//  nh.initNode();
-//  setupMsg(odometry, odometry_pub);
-//}
-//
-//void setupMsg(std_msgs::Float32MultiArray &msg, ros::Publisher &pub) {
-//  msg.layout.dim =
-//    (std_msgs::MultiArrayDimension *)
-//    malloc(sizeof(std_msgs::MultiArrayDimension) * 2);
-//  msg.layout.dim[0].label = "height";
-//  msg.layout.dim[0].size = messageLength;
-//  msg.layout.dim[0].stride = 1;
-//  msg.layout.data_offset = 0;
-//  msg.data = (float *)malloc(sizeof(int) * 8);
-//  msg.data_length = messageLength;
-//  nh.advertise(pub);
-//}
-//
-//// Data Rate Attenuation
-//long int lastPubTime = 0;
-//int dataRate  = 100; // ms or 10Hz
-//
-//void runROS() {
-//  long int time = millis();
-//  double dt = (time - lastPubTime);
-//  if (dt > dataRate) {
-//    sendMsg(odometry, odometry_pub);
-//    lastPubTime = time;
-//  }
-//  nh.spinOnce();  
-//}
-//
-//void sendMsg(std_msgs::Float32MultiArray &msg, ros::Publisher &pub) {
-//  msg.data[0] = left_odom;
-//  msg.data[1] = left_turn;
-//  msg.data[2] = right_odom;
-//  msg.data[3] = right_turn;
-//  pub.publish(&msg);
-//}
+void readSensor() {
+
+  left_odom = left_odom + 1;
+  left_turn = left_turn + 1;
+  right_odom = right_odom + 1;
+  right_turn = right_turn + 1;
+  
+}
+
+
+
 
