3cd3c8aea63a0b4695489c7436465f56bd5084cf
David Robinson <dvsrobinson@gmail.com>
2018-03-16 12:01:55 -0400

    initial odometry data collection

--- Arduino/odometry/odometry.ino
+++ Arduino/odometry/odometry.ino
@@ -16,31 +16,23 @@ static int messageLength = 4;
 std_msgs::Int16MultiArray odometry;
 ros::Publisher odometry_pub("odometry", &odometry);
 
-//long int lastPubTime = 0;
-//int dataRate  = 100; // ms or 10Hz
-
-
-Encoder left_enc(11, 12);
-Encoder right_enc(9, 10);
-int left_turn_pin = A0;
-int right_turn_pin = A1;
-
-int left_odom_prev = 0;
+Encoder left_enc(2, 5);
+Encoder right_enc(4, 3);
+long int left_odom_prev = 0;
 int left_odom = 0;
-int right_odom_prev = 0;
+long int right_odom_prev = 0;
 int right_odom = 0;
 
+static int left_turn_pin = A1;
+static int right_turn_pin = A0;
 int left_turn = 0;
 int right_turn = 0;
 
+float old_time = 0;
+
 bool debug = false; // Switch Serial Print / ROS Publisher
 
-const byte interruptPin = 9;
-
 void setup() {
-
-//  pinMode(interruptPin, INPUT_PULLUP);
-//  attachInterrupt(digitalPinToInterrupt(interruptPin), update_left, CHANGE);
 
   if (debug) {
     Serial.begin(9600);
@@ -50,8 +42,6 @@ void setup() {
     setupMsg(odometry, odometry_pub);
   }
 }
-
-
 
 void setupMsg(std_msgs::Int16MultiArray &msg, ros::Publisher &pub) {
   msg.layout.dim =
@@ -82,32 +72,26 @@ void loop() {
     Serial.println("");
   }
   else { // ROS Publisher
-
     sendMsg(odometry, odometry_pub);
     nh.spinOnce();
-
   }
-}
-
-void update_left() {
-  left_odom = left_odom + 1; // naieve increment
 }
 
 void read_sensors() {
 
   // report delta in odom
-  //  int left_new = left_enc.read();
-  //  int right_new = right_enc.read();
-  //
-  //  left_odom = left_new - left_odom_prev;
-  //  right_odom = right_new - right_odom_prev;
-  //
-  //  left_odom_prev = left_new;
-  //  right_odom_prev = right_new;
+  long int left_new = left_enc.read();
+  long int right_new = right_enc.read();
 
-  // report absolute value
-  left_odom = left_enc.read();
-  right_odom = right_enc.read();
+  float time_now = millis();
+
+  left_odom = (left_new - left_odom_prev);
+  right_odom = (right_new - right_odom_prev);
+
+  left_odom_prev = left_new;
+  right_odom_prev = right_new;
+
+  old_time = time_now;
 
   // get steering voltage 1.3 to 2.3V
   left_turn = analogRead(left_turn_pin);
