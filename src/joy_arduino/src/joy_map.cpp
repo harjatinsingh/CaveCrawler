dc60c1cc23a5c20163bbad8d98f49f0a17753c17
Unknown <mswenson@andrew.cmu.edu>
2018-04-09 00:43:45 -0400

    made it work with the husky bag file

--- src/joy_arduino/src/joy_map.cpp
+++ src/joy_arduino/src/joy_map.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <std_msgs/Int32MultiArray.h>
+#include <geometry_msgs/Twist.h>
 
 class JoyArduino
 {
@@ -8,9 +9,12 @@ class JoyArduino
 		JoyArduino();
 	private:
 		void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+		void controlCallback(const geometry_msgs::Twist::ConstPtr& msg);
+        bool joy_control;
 		ros::NodeHandle nh;
 		ros::Publisher ArduinoPub;
 		ros::Subscriber JoySub;
+		ros::Subscriber ackermannMsg;
 };
 
 
@@ -22,6 +26,7 @@ JoyArduino::JoyArduino()
 	//joystick topic
 	JoySub = nh.subscribe<sensor_msgs::Joy>("joy",10,&JoyArduino::joyCallback,this);
 
+    joy_control = true;
 }
 
 int ConvertToRange(float num)
@@ -34,6 +39,29 @@ int absoluteRange(float num)
 	return (((int)(num*100 +100))/2);
 }
 
+void JoyArduino::controlCallback(const geometry_msgs::Twist::ConstPtr& msg)
+{
+
+	std_msgs::Int32MultiArray PublishArray;
+	PublishArray.data.clear();
+
+    if(!joy_control){ //switch to autonomous control of robot
+
+        //linear velocity mapping
+        PublishArray.data.push_back(ConvertToRange(msg->linear.x));
+        
+        //steering mapping
+        PublishArray.data.push_back(ConvertToRange(msg->angular.z));
+        
+        //max robot speed
+        PublishArray.data.push_back(1);
+        PublishArray.data.push_back(1); //assume forward mode always for now
+    }
+
+	ArduinoPub.publish(PublishArray);
+
+}
+
 void JoyArduino::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
 	std_msgs::Int32MultiArray PublishArray;
@@ -41,49 +69,54 @@ void JoyArduino::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 
 
 	//map joystick axes to robot controls 
+    if(joy->buttons[3] == 1)
+    {
+        joy_control = !joy_control;
+    }
 
-	//linear velocity mapping
-	PublishArray.data.push_back(ConvertToRange(joy->axes[4]));
-	
-	//steering mapping
-	PublishArray.data.push_back(ConvertToRange(-1*joy->axes[3]));
-	
-	//max robot speed
-	PublishArray.data.push_back(absoluteRange(joy->axes[2]));
+    if(joy_control){
+        //linear velocity mapping
+        PublishArray.data.push_back(ConvertToRange(joy->axes[4]));
+        
+        //steering mapping
+        PublishArray.data.push_back(ConvertToRange(-1*joy->axes[3]));
+        
+        //max robot speed
+        PublishArray.data.push_back(absoluteRange(joy->axes[2]));
 
 
-	//map joystick buttons to robot control
-	if(joy->buttons[5] == 1)
-	{
-		PublishArray.data.push_back(1); //forward mode	
-	}
-	else if (joy->buttons[4] == 1)
-	{
-		PublishArray.data.push_back(2); //crab mode
-	}
-	else
-	{
-		PublishArray.data.push_back(0);//continue in previous mode
-	}
+        //map joystick buttons to robot control
+        if(joy->buttons[5] == 1)
+        {
+            PublishArray.data.push_back(1); //forward mode	
+        }
+        else if (joy->buttons[4] == 1)
+        {
+            PublishArray.data.push_back(2); //crab mode
+        }
+        else
+        {
+            PublishArray.data.push_back(0);//continue in previous mode
+        }
 
-	if(joy->buttons[8] == 1)	
-	{
-		PublishArray.data.push_back(1);	 //forward direction
-	}
-	else
-	{
-		PublishArray.data.push_back(0);//reverse direction
-	}
+        if(joy->buttons[8] == 1)	
+        {
+            PublishArray.data.push_back(1);	 //forward direction
+        }
+        else
+        {
+            PublishArray.data.push_back(0);//reverse direction
+        }
 
-	if(joy->buttons[6] == 1 && joy->buttons[7] == 1 && joy->buttons[9] == 1 &&joy->buttons[10] == 1)	
-	{
-		PublishArray.data.push_back(1);	//amplifiers reset
-	}
-	else
-	{
-		PublishArray.data.push_back(0);
-	}
-
+        if(joy->buttons[6] == 1 && joy->buttons[7] == 1 && joy->buttons[9] == 1 &&joy->buttons[10] == 1)	
+        {
+            PublishArray.data.push_back(1);	//amplifiers reset
+        }
+        else
+        {
+            PublishArray.data.push_back(0);
+        }
+    }
 	
 	ArduinoPub.publish(PublishArray);
 			
