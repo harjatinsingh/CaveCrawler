d1dc980f7d6255a3bb5ca73f05ac0767866442fa
hks95 <harikrishnan.suresh12@gmail.com>
2018-04-24 18:11:59 -0400

    fixed issues, now going to start the planner implementation

--- src/cavecrawler_2dnav/cavecrawler_planning/src/rrt_global_planner.cpp
+++ src/cavecrawler_2dnav/cavecrawler_planning/src/rrt_global_planner.cpp
@@ -26,6 +26,7 @@ void OmplGlobalPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* c
    {
         ros::NodeHandle private_nh("~/" + name);
         _costmap_ros = costmap_ros;
+
         _frame_id = "world";
         max_footprint_cost = 256;
         _costmap_model = new base_local_planner::CostmapModel(*_costmap_ros->getCostmap());
@@ -34,6 +35,14 @@ void OmplGlobalPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* c
         // private_nh.param("global_frame",  _frame_id, world);
         private_nh.param("allow_unknown", _allow_unknown, true);
 		// private_nh.param("max_footprint_cost", max_footprint_cost, 256);
+    // private_nh_.param("max_dist_between_pathframes", max_dist_between_pathframes_, 0.10);
+    //   private_nh_.param("max_footprint_cost", max_footprint_cost_, 256);
+    //   private_nh_.param("relative_validity_check_resolution", relative_validity_check_resolution_, 0.05);
+    //   private_nh_.param("interpolate_path", interpolate_path_, true);
+    //   private_nh_.param("publish_diagnostics", publish_diagnostics_, false);
+    //   private_nh_.param("Inscribed_radius", inscribed_radius_, 0.2);				// 6/27/2014
+    //   private_nh_.param("Circumscribed_radius", circumscribed_radius_, 0.25);		// 6/27/2014
+
         //get the tf prefix
         ros::NodeHandle prefix_nh;
         tf_prefix_ = tf::getPrefixParam(prefix_nh);
@@ -47,9 +56,10 @@ void OmplGlobalPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* c
     }
 }
 
-bool OmplGlobalPlanner::isStateValid(const oc::SpaceInformation *si,const ob::State *state)
+bool OmplGlobalPlanner::isStateValid(const ob::State *state)
 {
-    if (!si->satisfiesBounds(state))
+    bool inBound = _space->satisfiesBounds(state);
+    if (!inBound)
     {
         return false;
     }
@@ -69,88 +79,170 @@ bool OmplGlobalPlanner::isStateValid(const oc::SpaceInformation *si,const ob::St
 	return false;
 }
 
+	void OmplGlobalPlanner::PoseToPose2D(const geometry_msgs::Pose pose, geometry_msgs::Pose2D& pose2D)
+	{
+		// use tf-pkg to convert angles
+		tf::Pose pose_tf;
+		
+		// convert geometry_msgs::PoseStamped to tf::Pose
+		tf::poseMsgToTF(pose, pose_tf);
+		
+		// now get Euler-Angles from pose_tf
+		double useless_pitch, useless_roll, yaw;
+		pose_tf.getBasis().getEulerYPR(yaw, useless_pitch, useless_roll);
+		// normalize angle
+		yaw = angles::normalize_angle(yaw);
+		
+		// and set to pose2D
+		pose2D.x = pose.position.x;
+		pose2D.y = pose.position.y;
+		pose2D.theta = yaw;
+		
+		return;
+	}
 
 bool OmplGlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,  std::vector<geometry_msgs::PoseStamped>& plan )
 {
 
-  ROS_DEBUG("Got a start: %.2f, %.2f, and a goal: %.2f, %.2f", start.pose.position.x, start.pose.position.y, goal.pose.position.x, goal.pose.position.y);
+	// ROS_DEBUG("Got a start: %.2f, %.2f, and a goal: %.2f, %.2f", start.pose.position.x, start.pose.position.y, goal.pose.position.x, goal.pose.position.y);
+	// ROS_INFO_STREAM("start and goal is "<<start.pose.position.x<<" "<<start.pose.position.y<<" "<<goal.pose.position.x<<" "<<goal.pose.position.y);
+	plan.clear();
+	ros::NodeHandle n();
+	std::string global_frame = _frame_id;
 
- //  plan.clear();
- //  ros::NodeHandle n();
- //  std::string global_frame = _frame_id;
- //  ROS_INFO_STREAM("Main");
- //  // ompl::msg::noOutputHandler();
- //  // Create R3 space
- //  // ob::RealVectorBounds bounds(3);  // We are going to create a unit box from (-1,-1,-1) to (1,1,1)
- //  // bounds.setLow(0, -1.0);
- //  // bounds.setLow(1, -1.0);
- //  // bounds.setLow(2, -1.0);
- //  // bounds.setHigh(0, 1.0);
- //  // bounds.setHigh(1, 1.0);
- //  // bounds.setHigh(2, 1.0);
- //       ob::RealVectorBounds bounds(2);
- //      bounds.setLow(-1);
- //      bounds.setHigh(1);
- //  _space->as<ob::RealVectorStateSpace>()->setBounds(bounds);
- //  ob::SpaceInformationPtr si(new ob::SpaceInformation(_space));
+	costmap_2d::Costmap2D* costmap_ =  _costmap_ros->getCostmap(); 
+
+	tf::Stamped<tf::Pose> goal_tf;
+	tf::Stamped<tf::Pose> start_tf;
+
+	poseStampedMsgToTF(goal,goal_tf);
+	poseStampedMsgToTF(start,start_tf);
 
 
+	// get bounds from worldmap and set it to bounds for the planner
+	// as goal and map are set in same frame (checked above) we can directly
+	// get the extensions of the statespace from the map-prms		
+	ob::RealVectorBounds bounds(2);
+	double map_upperbound, map_lowerbound;
 
- //  // Set start and goal
- //  ob::ScopedState<ob::SE2StateSpace> robot_start(_space);//, // Planning from (0,0,0) to (0.8, 0.8, 0.8)
- //   robot_start->setX(-0.5);
- //  robot_start->setY(0.0);
- //  robot_start->setYaw(0.0);
- //  ob::ScopedState<ob::SE2StateSpace> robot_goal(_space);//, // Planning from (0,0,0) to (0.8, 0.8, 0.8)
- //   robot_goal->setX(0.5);
- //  robot_goal->setY(1.0);
- //  robot_start->setYaw(0.0);
+	//CHECK THIS IN THE FINAL START GOAL CONFIG. MIGHT HAVE TO CHANGE THE CONFIG STUFF
+	// get bounds for x coordinate
+	map_upperbound = costmap_->getSizeInMetersX() - costmap_->getOriginX();	// costmap_->getOriginX();
+	map_lowerbound = costmap_->getOriginX();											// map_upperbound - costmap_->getSizeInMetersX();
+	bounds.setHigh(0, map_upperbound);
+	bounds.setLow(0, map_lowerbound);
+	// ROS_WARN("Setting upper bound and lower bound of map x-coordinate to (%f, %f).", map_upperbound, map_lowerbound);
 
- // si->setStateValidityChecker(boost::bind(&OmplGlobalPlanner::isStateValid,this,_1));
- //  //si->setStateValidityChecker([&si](const ob::State *state) { return isStateValid(si.get(), state); });
- //  si->setStateValidityCheckingResolution(0.001); // dimensionless number as a fraction of the workspace size
- //  si->setup();
+	// get bounds for y coordinate
+	map_upperbound = costmap_->getSizeInMetersY() - costmap_->getOriginY();
+	map_lowerbound = costmap_->getOriginY();
+	bounds.setHigh(1, map_upperbound);
+	bounds.setLow(1, map_lowerbound);
+	// ROS_WARN("Setting upper bound and lower bound of map y-coordinate to (%f, %f).", map_upperbound, map_lowerbound);
 
- //  // Setup problem
- //  ob::ProblemDefinitionPtr pdef(new ob::ProblemDefinition(si));
- //  pdef->setStartAndGoalStates(robot_start.get(), robot_goal.get());
- //  pdef->setOptimizationObjective(ob::OptimizationObjectivePtr(new ob::PathLengthOptimizationObjective(si)));
+	// now set it to the planner
+	_space->as<ob::SE2StateSpace>()->setBounds(bounds);
+	// ROS_INFO_STREAM("SizeInMetersX "<<costmap_->getSizeInMetersX()<<" SizeInMetersY "<<costmap_->getSizeInMetersY());
+	// ROS_INFO_STREAM("OriginX "<<costmap_->getOriginX()<<" OriginY() "<<costmap_->getOriginY());											// map_upperbound - costmap_->getSizeInMetersY();		
+	// now create instance to ompl setup
+	og::SimpleSetup ss(_space);
+	// get SpaceInformationPointer from simple_setup (initialized in makePlan routine)
+	ob::SpaceInformationPtr si = ss.getSpaceInformation();
+ 	// ob::SpaceInformationPtr si(new ob::SpaceInformation(_space));
+	// set state validity checker
+	si->setStateValidityChecker(boost::bind(&OmplGlobalPlanner::isStateValid,this,_1));
+	// // set validity checking resolution
+	si->setStateValidityCheckingResolution(0.05); // dimensionless number as a fraction of the workspace size
+  	// si->setup();
+    double yaw, pitch, roll;
+    tf::Pose pose_tf;
 
- //  // Setup planner
- //  // boost::shared_ptr<ompl::geometric::RRTstar> rrtstar(new ompl::geometric::RRTstar(si));
- //  // ob::PlannerPtr planner = rrtstar;
+    // Define problem:
+    ob::ScopedState<> ompl_start(_space);
+    ompl_start[0] = start.pose.position.x;
+    ompl_start[1] = start.pose.position.y;
 
- //  auto planner(std::make_shared<og::RRTConnect>(si));
+    tf::poseMsgToTF(start.pose, pose_tf);
+    pose_tf.getBasis().getEulerYPR(yaw, pitch, roll);
+    
+    ompl_start[2] = yaw;
+	// check whether this satisfies the bounds of the manifold
+	bool inBound = _space->satisfiesBounds(ompl_start->as<ob::SE2StateSpace::StateType>());
+	if(!inBound)
+	{
+		ROS_ERROR("Start Pose lies outside the bounds of the map - Aborting Planner ");
+		return false;
+	}
 
- //    planner->setProblemDefinition(pdef);
- //    planner->setup();
- //    ob::PlannerStatus solved = planner->ob::Planner::solve(0.0);
+    ob::ScopedState<> ompl_goal(_space);
+    ompl_goal[0] = goal.pose.position.x;
+    ompl_goal[1] = goal.pose.position.y;
 
- //    // if(solved == ob::PlannerStatus::EXACT_SOLUTION) 
- //    if (solved)
- //    {
- //      // boost::shared_ptr<og::PathGeometric> path = boost::static_pointer_cast<og::PathGeometric>(planner->getProblemDefinition()->getSolutionPath()); 
- //      ob::PathPtr path = pdef->getSolutionPath();
- //      // ob::Cost cost = planner->getProblemDefinition()->getOptimizationObjective()->pathCost(path.get());
- //      ROS_INFO_STREAM("Found solution haha");// with cost: "<<cost.value());
- //      // pub_path_marker.publish(planning_common::visualization_utils::GetMarker(*path, 0.01));
- //      ob::PlannerData data(si);
- //      planner->getPlannerData(data);
- //      // pub_graph_marker.publish(planning_common::visualization_utils::GetGraph(data, 100, 0.01, 0, 0, 1, 0.3));
- //    }
- //    else  
- //    {
- //      ROS_INFO_STREAM("No Solution Found");
- //      ob::PlannerData data(si);
- //      planner->getPlannerData(data);
- //      // pub_graph_marker.publish(planning_common::visualization_utils::GetGraph(data, 100, 0.01, 0, 0, 1, 0.3));
- //    }
- //    // pub_obstacles_marker_array.publish(obstacle_set.GetMarkerArray(1, 0, 0, 0.7));
+    tf::poseMsgToTF(goal.pose, pose_tf);
+    pose_tf.getBasis().getEulerYPR(yaw, pitch, roll);
 
- //    planner->clear();
- //    pdef->clearSolutionPaths();
- //    ros::Duration(0.5).sleep();
-  return true;
+    ompl_goal[2] = yaw;
+	// before starting planner -> check whether target configuration is free
+	// Goal position check
+	int sample_costs =  _costmap_model->footprintCost(goal.pose.position.x, goal.pose.position.y, yaw,_costmap_ros->getRobotFootprint());
 
-}
+	if ((sample_costs < 0.0) || (sample_costs > max_footprint_cost))
+	{
+		ROS_ERROR("Collision on target: Planning aborted! Change target position.");
+		return false;
+	}
+	
+	// check whether this satisfies the bounds of the manifold
+	inBound = _space->satisfiesBounds(ompl_goal->as<ob::SE2StateSpace::StateType>());
+	if(!inBound)
+	{
+		ROS_ERROR("Target Pose lies outside the bounds of the map - Aborting Planner ");
+		return false;
+	}
+	
+	// set start and goal state to planner
+	ss.setStartAndGoalStates(ompl_start, ompl_goal);
+	ROS_INFO_STREAM("going to setup problem");
+	//  // Setup problem
+	//  ob::ProblemDefinitionPtr pdef(new ob::ProblemDefinition(si));
+	//  pdef->setStartAndGoalStates(robot_start.get(), robot_goal.get());
+	//  pdef->setOptimizationObjective(ob::OptimizationObjectivePtr(new ob::PathLengthOptimizationObjective(si)));
+
+	//  // Setup planner
+	//  // boost::shared_ptr<ompl::geometric::RRTstar> rrtstar(new ompl::geometric::RRTstar(si));
+	//  // ob::PlannerPtr planner = rrtstar;
+
+	//  auto planner(std::make_shared<og::RRTConnect>(si));
+
+	//    planner->setProblemDefinition(pdef);
+	//    planner->setup();
+	//    ob::PlannerStatus solved = planner->ob::Planner::solve(0.0);
+
+	//    // if(solved == ob::PlannerStatus::EXACT_SOLUTION) 
+	//    if (solved)
+	//    {
+	//      // boost::shared_ptr<og::PathGeometric> path = boost::static_pointer_cast<og::PathGeometric>(planner->getProblemDefinition()->getSolutionPath()); 
+	//      ob::PathPtr path = pdef->getSolutionPath();
+	//      // ob::Cost cost = planner->getProblemDefinition()->getOptimizationObjective()->pathCost(path.get());
+	//      ROS_INFO_STREAM("Found solution haha");// with cost: "<<cost.value());
+	//      // pub_path_marker.publish(planning_common::visualization_utils::GetMarker(*path, 0.01));
+	//      ob::PlannerData data(si);
+	//      planner->getPlannerData(data);
+	//      // pub_graph_marker.publish(planning_common::visualization_utils::GetGraph(data, 100, 0.01, 0, 0, 1, 0.3));
+	//    }
+	//    else  
+	//    {
+	//      ROS_INFO_STREAM("No Solution Found");
+	//      ob::PlannerData data(si);
+	//      planner->getPlannerData(data);
+	//      // pub_graph_marker.publish(planning_common::visualization_utils::GetGraph(data, 100, 0.01, 0, 0, 1, 0.3));
+	//    }
+	//    // pub_obstacles_marker_array.publish(obstacle_set.GetMarkerArray(1, 0, 0, 0.7));
+
+	//    planner->clear();
+	//    pdef->clearSolutionPaths();
+	//    ros::Duration(0.5).sleep();
+	return true;
+
+	}
 };
