6b875d5ae72c4a7302183a4f2e5e9ba295411996
harjatinsingh <harjatin@gmail.com>
2018-04-29 00:34:12 -0400

    publishing cmd vel to robot

--- src/joy_arduino/src/joy_map.cpp
+++ src/joy_arduino/src/joy_map.cpp
@@ -10,10 +10,11 @@ class JoyArduino
 	private:
 		void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 		void controlCallback(const geometry_msgs::Twist::ConstPtr& msg);
-        bool joy_control;
+		bool joy_control;
 		ros::NodeHandle nh;
 		ros::Publisher ArduinoPub;
 		ros::Subscriber JoySub;
+		ros::Subscriber cmdVelSub;
 		ros::Subscriber ackermannMsg;
 };
 
@@ -22,11 +23,13 @@ JoyArduino::JoyArduino()
 {
 	//publish bot commands
 	ArduinoPub = nh.advertise<std_msgs::Int32MultiArray>("bot",100);
-	
+
 	//joystick topic
 	JoySub = nh.subscribe<sensor_msgs::Joy>("joy",10,&JoyArduino::joyCallback,this);
 
-    joy_control = true;
+	cmdVelSub = nh.subscribe<geometry_msgs::Twist>("cmd_vel",10,&JoyArduino::controlCallback,this);
+
+	joy_control = true;
 }
 
 int ConvertToRange(float num)
@@ -42,22 +45,28 @@ int absoluteRange(float num)
 void JoyArduino::controlCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
 
-    if(!joy_control || true){ //switch to autonomous control of robot
-        std_msgs::Int32MultiArray PublishArray;
-        PublishArray.data.clear();
+	if(!joy_control){ //switch to autonomous control of robot
+		ROS_WARN("Trying to drive robot autonomously");
+		std_msgs::Int32MultiArray PublishArray;
+		PublishArray.data.clear();
 
-        //linear velocity mapping
-        PublishArray.data.push_back(ConvertToRange(msg->linear.x));
-        
-        //steering mapping
-        PublishArray.data.push_back(ConvertToRange(msg->angular.z));
-        
-        //max robot speed
-        PublishArray.data.push_back(1);
-        PublishArray.data.push_back(1); //assume forward mode always for now
+		//linear velocity mapping
+		ROS_INFO_STREAM("forward vel");
+		ROS_INFO_STREAM(msg->linear.x);
+		PublishArray.data.push_back(ConvertToRange(msg->linear.x));
 
-        ArduinoPub.publish(PublishArray);
-    }
+		//steering mapping
+		PublishArray.data.push_back(ConvertToRange(msg->angular.z));
+
+		//max robot speed
+		PublishArray.data.push_back(absoluteRange(-.75));
+
+		PublishArray.data.push_back(0);//continue in previous mode
+		PublishArray.data.push_back(0);//continue in previous mode
+
+		ROS_INFO_STREAM(PublishArray);
+		ArduinoPub.publish(PublishArray);
+	}
 
 }
 
@@ -66,66 +75,75 @@ void JoyArduino::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 	std_msgs::Int32MultiArray PublishArray;
 	PublishArray.data.clear();
 
-
 	//map joystick axes to robot controls 
-    if(joy->buttons[3] == 1)
-    {
-        ROS_WARN("Toggling control of robot");
-        joy_control = !joy_control;
-    }
+	if(joy->buttons[3] == 1)
+	{
+		joy_control = !joy_control;
+		if( joy_control){
+			ROS_WARN("Toggling control of robot to joystick");
+		}
+		else{ 
+			ROS_WARN("Toggling to autonomous control");
+		}
 
-    if(joy_control){
-        //linear velocity mapping
-        PublishArray.data.push_back(ConvertToRange(joy->axes[4]));
-        
-        //steering mapping
-        PublishArray.data.push_back(ConvertToRange(-1*joy->axes[3]));
-        
-        //max robot speed
-        PublishArray.data.push_back(absoluteRange(joy->axes[2]));
+	}
+
+	if(joy_control){
+		ROS_WARN("pushing joystick control");
+		//linear velocity mapping
+		ROS_INFO_STREAM("forward vel");
+		ROS_INFO_STREAM(joy->axes[4]);
+		PublishArray.data.push_back(ConvertToRange(joy->axes[4]));
+
+		//steering mapping
+		PublishArray.data.push_back(ConvertToRange(-1*joy->axes[3]));
+
+		//max robot speed
+		PublishArray.data.push_back(absoluteRange(joy->axes[2]));
 
 
-        //map joystick buttons to robot control
-        if(joy->buttons[5] == 1)
-        {
-            PublishArray.data.push_back(1); //forward mode	
-        }
-        else if (joy->buttons[4] == 1)
-        {
-            PublishArray.data.push_back(2); //crab mode
-        }
-        else
-        {
-            PublishArray.data.push_back(0);//continue in previous mode
-        }
+		//map joystick buttons to robot control
+		if(joy->buttons[5] == 1)
+		{
+			PublishArray.data.push_back(1); //forward mode	
+		}
+		else if (joy->buttons[4] == 1)
+		{
+			PublishArray.data.push_back(2); //crab mode
+		}
+		else
+		{
+			PublishArray.data.push_back(0);//continue in previous mode
+		}
 
-        if(joy->buttons[8] == 1)	
-        {
-            PublishArray.data.push_back(1);	 //forward direction
-        }
-        else
-        {
-            PublishArray.data.push_back(0);//reverse direction
-        }
+		if(joy->buttons[8] == 1)	
+		{
+			PublishArray.data.push_back(1);	 //forward direction
+		}
+		else
+		{
+			PublishArray.data.push_back(0);//reverse direction
+		}
 
-        if(joy->buttons[6] == 1 && joy->buttons[7] == 1 && joy->buttons[9] == 1 &&joy->buttons[10] == 1)	
-        {
-            PublishArray.data.push_back(1);	//amplifiers reset
-        }
-        else
-        {
-            PublishArray.data.push_back(0);
-        }
-    }
-	
+		if(joy->buttons[6] == 1 && joy->buttons[7] == 1 && joy->buttons[9] == 1 &&joy->buttons[10] == 1)	
+		{
+			PublishArray.data.push_back(1);	//amplifiers reset
+		}
+		else
+		{
+			PublishArray.data.push_back(0);
+		}
+		ROS_INFO_STREAM(PublishArray);
+
 	ArduinoPub.publish(PublishArray);
-			
+	}
+
 }
 
 
 int main(int argc,char** argv)
 {
-    ROS_WARN("Starting joy arduino");
+	ROS_WARN("Starting joy arduino");
 	ros::init(argc,argv,"joy_arduno");
 	JoyArduino joy_arduno;
 	ros::spin();
