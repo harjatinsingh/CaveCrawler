677d476c1474efab098eb8733bc2b6326051fa03
David Robinson <dvsrobinson@gmail.com>
2018-04-29 00:47:21 -0400

    hi my name is david and this is my ekf

--- src/localization/src/odom_cvt.py
+++ src/localization/src/odom_cvt.py
@@ -5,30 +5,28 @@ import tf
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Int16, Int16MultiArray
 from geometry_msgs.msg import Point, Pose, Quaternion, QuaternionStamped, Twist, Vector3
+from sensor_msgs.msg import Imu
 import pdb
 
 wheel_dist = 1
 w = 0.9
 
 def update_odom(data):
-    raw_odom = data.data
+    raw_odom = data
     global left_odom
     global right_odom
     global left_turn_raw
     global right_turn_raw
-    left_odom = raw_odom.quaternion.x # raw_odom[0]
-    right_odom = raw_odom.quaternion.y # raw_odom[1]
-    left_turn_raw = raw_odom.quaternion.z # raw_odom[2]
-    right_turn_raw = raw_odom.quaternion.w #raw_odom[3]
+    left_odom = raw_odom.quaternion.x
+    right_odom = raw_odom.quaternion.y
+    left_turn_raw = raw_odom.quaternion.z
+    right_turn_raw = raw_odom.quaternion.w
 
-    scale = 6e-3# 6e-6
-    left_odom = left_odom * scale
-    right_odom = right_odom * scale
+    left_odom = left_odom * odometry_scale
+    right_odom = right_odom * odometry_scale
 
     left_turn = (-0.0968 * left_turn_raw + 27.1744)*math.pi/180
     right_turn = (0.1112 * right_turn_raw - 31.0661)*math.pi/180
-
-    # print "left_odom", left_odom, "right_odom", right_odom
 
     # get forward velocity
     v = (left_odom+right_odom)/2;
@@ -52,7 +50,7 @@ def update_odom(data):
     if abs(turn_centre) < 1e5 and not v == 0:
         A = turn_centre**2 - v**2;
         try:
-            d_heading = direction*math.atan2(v,math.sqrt(A));
+            d_heading = -direction*math.atan2(v,math.sqrt(A));
         except:
             d_heading = 0;
     else:
@@ -75,15 +73,48 @@ def update_odom(data):
     global odom_pub
     odom_pub.publish(odom)
 
+    # debugging
+    # print "v: %0.2f" % v, " omega: %0.2f" % d_heading
+
+
+def update_imu(data):
+    new_msg = data
+
+    q = new_msg.orientation
+    roll, pitch, yaw = tf.transformations.euler_from_quaternion([q.x,q.y,q.z,q.w])
+
+    # if abs(yaw) > math.pi:
+    #     print "TRIGGERED!"
+    #     # new_yaw = -yaw
+    #     yaw = -yaw
+    # else:
+    #     # new_yaw = yaw
+    #     yaw = yaw # + math.pi
+
+    # print "roll: %0.2f" % (roll*180/math.pi), "%0.2f" % (pitch*180/math.pi), "%0.2f" % (yaw*180/math.pi)
+
+    # q = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+    # new_msg.orientation.x = q[0]
+    # new_msg.orientation.y = q[1]
+    # new_msg.orientation.z = q[2]
+    # new_msg.orientation.w = q[3]
+    # imu_pub.publish(new_msg)
+
 
 def main():
 
     rospy.init_node('odom_cvt')
 
+    global odometry_scale
+    odometry_scale = rospy.get_param('odometry_scale')
+
     global odom_pub
     odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
-    incoming_odom = rospy.Subscriber('/arduino/raw_odometry', QuaternionStamped, update_odom)
+    incoming_odom = rospy.Subscriber('/raw_odometry', QuaternionStamped, update_odom)
 
+    # global imu_pub
+    # imu_pub = rospy.Publisher("/imu/data", Imu, queue_size=50)
+    # incoming_imu = rospy.Subscriber('/imu/raw', Imu, update_imu)
 
     r = rospy.Rate(1.0)
     while not rospy.is_shutdown():
